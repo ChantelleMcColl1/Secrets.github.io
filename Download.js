057422b5137354f7f0cab325e2e617f601ef195c
ChantelleMcColl1 <102559270+ChantelleMcColl1@users.noreply.github.com>
2022-04-02 17:06:24 +1100

    Update Download.js

--- Download.js
+++ Download.js
@@ -1,5 +1,5 @@
-
-var doc = new jsPDF (window.jsPDF = window.jspdf.jsPDF) ;
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.min.js"></script>
+var doc = new jsPDF () ;
 var specialElementHandlers = {
     '#print-btn': function (element, renderer) {
         return true;
